06d15da0ec189727c30292e804a3d1fe65143fe6
jlreis <jorge_reis@alu.ufc.br>
2019-03-08 13:10:38 -0300

    fix

--- RoundRobinArbiter.v
+++ RoundRobinArbiter.v
@@ -19,8 +19,8 @@
 // 
 //////////////////////////////////////////////////////////////////////////////////
 
-
-module RoundRobinArbiter #(parameter size=8)
+`include"defines.vh"
+module RoundRobinArbiter #(parameter size=`NPORT)
 (
 input [size-1:0] requests,
 input enable,
@@ -28,33 +28,41 @@ output isOutputSelected,
 output reg [$clog2(size)-1:0] selectedOutput 
  );
 
-reg [$clog2(size)-1:0] lastport;
-reg [$clog2(size)-1:0] selectedPort =0;
+reg [$clog2(size)-1:0] lastport=0;
+reg [$clog2(size)-1:0] selectedPort=0 ;
 reg [$clog2(size)-1:0] requestCheck;
-reg i;
+integer i;
 always@(posedge enable)
-begin 
-        requestCheck=lastport;
-        selectedPort=lastport;
-            for (i = 0; i < requests; i = i +1) 
-                        begin
-                            if(requestCheck==size-1)
-                                begin
-                                    requestCheck=0;
-                                end
-                            else
-                                begin
-                                    requestCheck=requestCheck+1;
-                                end
-                            if(requests[requestCheck]==1)
-                                begin
-                                    selectedPort=requestCheck;
-                                end
-                        end     
-            
-            lastport<=selectedPort;
-            selectedOutput=selectedPort;
+    begin 
+    requestCheck=lastport;
+//    $display("The value of 'requestCheck' out of loop is %d in %g",requestCheck,$time);
+//    $display("The value of 'selectedPort' out of loop is %d in %g",selectedPort,$time);
+//    $display("The value of 'lastPort' out of loop is %d in %g",lastport,$time);
 
-end
-            assign isOutputSelected=enable;
+    selectedPort=lastport;
+    for (i = 0; i < size; i = i +1) 
+        begin
+        if(requestCheck==size-1)
+            begin
+            requestCheck=0;
+ //            $display("The value of 'requestCheck' in if is %d in %g",requestCheck,$time);
+            end
+        else
+            begin
+            requestCheck=requestCheck+1;
+//            $display("The value of 'requestCheck' in else is %d in %g",requestCheck,$time);
+            end
+            
+        if(requests[requestCheck]==1)
+            begin
+            selectedPort=requestCheck;
+            i=size;//simula o quit do vhdl
+//             $display("The value of 'requestCheck' in if2 is %d in %g",requestCheck,$time);
+            end
+        end     
+    lastport<=selectedPort;
+    selectedOutput<=selectedPort;
+    end
+    
+    assign isOutputSelected=enable;
 endmodule
